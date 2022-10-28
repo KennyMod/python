3ab9d2c3b815d8b06fe1caeb3c2fb0a2fe52eb76
Kenechukwu Modebelu <kmodebelu@gmail.com>
2022-10-29 01:45:04 +0200

    Opposite number

---
+++ 8kyu/codewars3.py
@@ -0,0 +1,30 @@
+#Opposite number
+
+#Very simple, given an integer or a floating-point number, find its opposite.
+
+#Examples:
+
+#1: -1
+#14: -14
+#-34: 34
+
+#(opposite(1), -1);
+#(opposite(0), 0);
+#(opposite(4.25), -4.25);
+#(opposite(3.3333333), -3.3333333);
+#(opposite(-12525220.3325), 12525220.3325);
+#(opposite(-5), 5);
+
+def opposite(number):
+  # your solution here
+        return -number
+
+
+print(opposite(1)) # -1);
+print(opposite(0)) # 0);
+print(opposite(4.25)) # -4.25);
+print(opposite(3.3333333)) # -3.3333333);
+print(opposite(-12525220.3325)) # 12525220.3325);
+print(opposite(-5)) # 5);
+
+
