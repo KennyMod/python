a43abc29578883ea7bbed9e3d8abf7a351bf4e39
Kenechukwu Modebelu <kmodebelu@gmail.com>
2022-10-30 01:13:38 +0200

    changed my file to python

---
+++ 8kyu/codewars1.py
@@ -0,0 +1,25 @@
+# Multiply
+
+# This code does not execute properly. Try to figure out why.
+
+#Solution
+
+#integers, positive, negative floats
+#return the multiplication of 2 parameter
+
+#multiply(1,1), 1);
+#multiply(2,1), 2);
+#multiply(2,2), 4);
+#multiply(3,5), 15);
+
+# multiply both parameter
+#return the output
+
+def multiply(a, b):
+    return a * b
+
+
+print(multiply(1,1)) # 1);
+print(multiply(2,1)) # 2);
+print(multiply(2,2)) # 4);
+print(multiply(3,5)) # 15);
